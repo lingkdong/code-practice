022a72b973acf4c32aba71836508719048775f65
dongxin <2524964431@qq.com>
2019-02-25 11:06:39 +0800

    add

---
+++ src/test/java/com/code/codepractice/ProgramTest.java
@@ -0,0 +1,14 @@
+package com.code.codepractice;
+
+import org.junit.Test;
+
+/**
+ * @Author: dongxin
+ * @Date: 2019/2/25 10:41
+ **/
+public class ProgramTest {
+    @Test
+    public void test(){
+        System.out.println(222);
+    }
+}
